7d52eb906c87d30d692eb05025f576dfefc354db
OksanaSanag <oksanasanag@gmail.com>
2019-12-16 13:46:34 +0200

    3. store. (added a class Store to hide the state)

--- redux.js
+++ redux.js
@@ -1,4 +1,4 @@
-let state = 0;
+// let state = 0; видалили
 
 function updateState(state, action) {
     if (action.type === 'INCREMENT') {
@@ -10,14 +10,30 @@ function updateState(state, action) {
     }
 }
 
+class Store { //ховаємо стан аппки від загального доступу за допомогою классу
+    constructor(updateState, state) { // <- state передається параметром, бо його буде визначати користувач
+        this._updateState = updateState; //ф-цію передаємо параметром, а не методом, щоб не прив'язувати клас до конкретного випадку (до ф-ції нашого лічильника) 
+        this._state = state; //назви особистих властивостей починаються з _ , їх краще не чіпати потім
+    }
+
+    get state() {// визначаєм геттер для того, щоб мати доступ до зміни state
+        return this._state;
+    }
+    update(action) { //метод, за допомогою якого можна буде взаємодіяти зі станом. Метод не вертає зн-ня state, він лише каже Store, що відбулася якась дія і що state треба змінити
+        this._state = this._updateState(this._state, action)
+    }
+}
+
+const store = new Store(updateState, 0);
+
 const incrementAction = { type: 'INCREMENT', amount: 5 };
 const decrementAction = { type: 'DECREMENT', amount: 3 };
 
-state = updateState(state, incrementAction);
-console.log(state);
+store.update(incrementAction); //видалили змінну state = , змінили updateState на метод store.update, і прибрали аргумент state
+console.log(store.state);
 
-state = updateState(state, decrementAction);
-console.log(state);
+store.update(decrementAction);
+console.log(store.state);
 
-state = updateState(state, {});
-console.log(state); 
+store.update({});
+console.log(store.state); 
