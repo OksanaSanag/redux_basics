7fd6a01506cc9399bf2592f06946936bbb732d27
OksanaSanag <oksanasanag@gmail.com>
2019-12-16 14:37:05 +0200

    4. subscribe

--- redux.js
+++ redux.js
@@ -1,4 +1,3 @@
-// let state = 0; видалили
 
 function updateState(state, action) {
     if (action.type === 'INCREMENT') {
@@ -10,18 +9,26 @@ function updateState(state, action) {
     }
 }
 
-class Store { //ховаємо стан аппки від загального доступу за допомогою классу
-    constructor(updateState, state) { // <- state передається параметром, бо його буде визначати користувач
-        this._updateState = updateState; //ф-цію передаємо параметром, а не методом, щоб не прив'язувати клас до конкретного випадку (до ф-ції нашого лічильника) 
-        this._state = state; //назви особистих властивостей починаються з _ , їх краще не чіпати потім
+class Store { 
+    constructor(updateState, state) { 
+        this._updateState = updateState; 
+        this._state = state; 
+        this._callbacks = []; // тут зберігатимемо список всіх колбеків
     }
 
-    get state() {// визначаєм геттер для того, щоб мати доступ до зміни state
+    get state() {
         return this._state;
     }
-    update(action) { //метод, за допомогою якого можна буде взаємодіяти зі станом. Метод не вертає зн-ня state, він лише каже Store, що відбулася якась дія і що state треба змінити
-        this._state = this._updateState(this._state, action)
-    }
+    
+    update(action) { 
+        this._state = this._updateState(this._state, action);
+        this._callbacks.forEach(callback => callback()); // store викликає ф-ції callback для всіх частин апп, які підписані на зміну state
+    } // не передаємо ніяких параметрів у ці ф-ції ^ , бо ми лише повідомляємо підписників про зміну стану
+    
+    subscribe(callback) { // підписка на зміну state (всі частини апп, які мусять знати про зміну state передадуть в Store ф-цію зворотнього виклику)
+        this._callbacks.push(callback);
+        return () => this._callbacks = this._callbacks.filter(cb => cb !== callback); // видаляє з масиву передану раніше ф-цію
+    } // тобто вертає функцію відписки
 }
 
 const store = new Store(updateState, 0);
@@ -29,11 +36,10 @@ const store = new Store(updateState, 0);
 const incrementAction = { type: 'INCREMENT', amount: 5 };
 const decrementAction = { type: 'DECREMENT', amount: 3 };
 
-store.update(incrementAction); //видалили змінну state = , змінили updateState на метод store.update, і прибрали аргумент state
-console.log(store.state);
+const unsibscribe = store.subscribe(() => console.log('State changed 1:', store.state)); // перший підписник
+store.subscribe(() => console.log('State changed 2:', store.state)); // другий підписник
 
+store.update(incrementAction);
+unsibscribe(); // перший підписник відписався від змін стану після першої зміни
 store.update(decrementAction);
-console.log(store.state);
-
 store.update({});
-console.log(store.state); 
